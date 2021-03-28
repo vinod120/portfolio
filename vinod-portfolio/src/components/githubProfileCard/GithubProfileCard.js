af628769ffef92f3f85b35b3b45eb4027e492ed4
vinod120 <vinodgate2016@gmail.com>
2021-03-28 12:14:43 +0530

    files changed

---
+++ vinod-portfolio/src/components/githubProfileCard/GithubProfileCard.js
@@ -0,0 +1,44 @@
+﻿import React from "react";
+import "./GithubProfileCard.css";
+import SocialMedia from "../../components/socialMedia/SocialMedia";
+import {contactInfo} from "../../portfolio";
+import emoji from "react-easy-emoji";
+import {Fade} from "react-reveal";
+
+export default function GithubProfileCard({prof}) {
+  if (prof.hireable === true) {
+    prof.hireable = "Yes";
+  } else {
+    prof.hireable = "Yes";
+  }
+  return (
+    <Fade bottom duration={1000} distance="20px">
+      <div className="main" id="contact">
+        <h1 className="prof-title">Reach Out to me!</h1>
+        <div className="row">
+          <div className="main-content-profile">
+            <div className="blog-header">
+              <p className="subTitle blog-subtitle">{contactInfo.subtitle}</p>
+              <p className="subTitle blog-subtitle">{contactInfo.title}</p>
+              <p>{contactInfo.number}</p>
+              <p>{contactInfo.email_address}</p>
+            </div>
+            <div className="opp-div">
+              <span className="desc-prof">
+                Open for opportunities: {prof.hireable}
+              </span>
+            </div>
+            <SocialMedia />
+          </div>
+          <div className="image-content-profile">
+            <img
+              src={prof.avatarUrl}
+              alt={prof.name}
+              className="profile-image"
+            />
+          </div>
+        </div>
+      </div>
+    </Fade>
+  );
+}
